50d80935b1e06c9040e2b593dd09366b73d50951
Kusal Wijekoon <iamkusalwijekoon@yahoo.com>
2024-06-11 20:14:35 +0530

    auth api updated

--- src/api/authSlice.ts
+++ src/api/authSlice.ts
@@ -4,7 +4,7 @@ export const authApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         login: builder.mutation({
             query: (credentials) => ({
-                url: '/user/login',
+                url: '/auth/librarian/login',
                 method: 'POST',
                 body: credentials
             })
