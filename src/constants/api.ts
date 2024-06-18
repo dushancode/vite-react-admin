f918efb89be48b4b4bacb7160bb4ea2614be22c5
Kusal Wijekoon <iamkusalwijekoon@yahoo.com>
2024-06-19 05:08:38 +0530

    api  base url

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,2 +1,7 @@
-
-export const BASE_URL = "http://localhost:5002"
+// Local
+// export const BASE_URL = "http://localhost:5002"
+// live
+// live
+// export const BASE_URL = "http://13.215.35.0:5002/api"
+// live
+export const BASE_URL = "http://13.215.35.0:5002
